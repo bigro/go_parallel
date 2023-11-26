79c1ab0dae68aa88938f5669fcff61adcb0bf05f
Yuuki Ooguro <kitkat.jaja@gmail.com>
2023-11-26 19:57:04 +0900

    ライブロック

--- main_test.go
+++ main_test.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"sync"
+	"sync/atomic"
 	"testing"
 	"time"
 )
@@ -32,4 +34,63 @@ func Test_Deadlock(t *testing.T) {
 	go printSum(&a, &b)
 	go printSum(&b, &a)
 	wg.Wait()
+}
+
+// ライブロックを引き起こす並行処理
+// 対面で歩いていてよける方向が同じでお見合いになる事象をシミュレート
+func Test_Livelock(t *testing.T) {
+	cadence := sync.NewCond(&sync.Mutex{})
+	go func() {
+		for range time.Tick(1*time.Microsecond) {
+			cadence.Broadcast()
+		}
+	}()
+
+	// 人間歩く歩調をシミュレート
+	takeStep := func() {
+		cadence.L.Lock()
+		cadence.Wait()
+		cadence.L.Unlock()
+	}
+
+	// 方向を指定して避けようとする
+	tryDir := func(dirName string, dir *int32, out *bytes.Buffer) bool {
+		fmt.Fprintf(out, " %v", dirName)
+		atomic.AddInt32(dir, 1)
+		takeStep()
+		if atomic.LoadInt32(dir) == 1 {
+			fmt.Fprint(out, ". Sucess!")
+			return true
+		}
+		takeStep()
+		// その方向に避けようとしてる人が他にもいれば歩くのを諦める
+		atomic.AddInt32(dir, -1)
+		return false
+	}
+
+	var left, right int32
+	// 左によける
+	tryLeft := func(out *bytes.Buffer) bool { return tryDir("left", &left, out) }
+	// 右によける
+	tryRight := func(out *bytes.Buffer) bool { return tryDir("right", &right, out) }
+
+	walk := func(walking * sync.WaitGroup, name string) {
+		var out bytes.Buffer
+		defer func() { fmt.Println(out.String()) }()
+		defer walking.Done()
+		fmt.Fprintf(&out, "%v is trying to scoot:", name)
+		for i := 0; i < 5; i++ {
+			// まず左に避けようとして、失敗したら右に避けようとする
+			if tryLeft(&out) || tryRight(&out) {
+				return
+			}
+		}
+		fmt.Fprintf(&out, "\n%v tosses her hands up in exasperation!", name)
+	}
+
+	var peopleInHallway sync.WaitGroup
+	peopleInHallway.Add(2)
+	go walk(&peopleInHallway, "Alice")
+	go walk(&peopleInHallway, "Barbara")
+	peopleInHallway.Wait()
 }
