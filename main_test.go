f3594b9ab335b662c2acec9fc4748f8fad43273f
Yuuki Ooguro <kitkat.jaja@gmail.com>
2023-11-28 23:31:20 +0900

    WaitGroupのループ時の使い方

--- main_test.go
+++ main_test.go
@@ -303,3 +303,18 @@ func Test_WaitGroup(t *testing.T) {
 	wg.Wait()
 	fmt.Println("All goroutines complete")
 }
+
+func Test_WaitGroupLoop(t *testing.T) {
+	hello := func(wg *sync.WaitGroup, id int)  {
+		defer wg.Done()
+		fmt.Printf("Hello from %v!\n", id)
+	}
+
+	const numGreeters = 5
+	var wg sync.WaitGroup
+	wg.Add(numGreeters)
+	for i := 0; i < numGreeters; i++ {
+		go hello(&wg, i+1)
+	}
+	wg.Wait()
+}
